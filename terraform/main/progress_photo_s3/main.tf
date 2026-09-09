0ef193d9115fa78e607f85177cd1d183bf7fb3bd
Edgar Mevaa <mevaed4@gmail.com>
2026-09-09 19:43:23 +0100

    CI: testing the CI

--- terraform/main/progress_photo_s3/main.tf
+++ terraform/main/progress_photo_s3/main.tf
@@ -1,21 +1,22 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.progress_photo_bucket_name 
-  tags = var.tags
+resource "aws_s3_bucket" "progress_photos" {
+  bucket = var.progress_photo_bucket_name
+  tags   = var.tags
 }
 
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_versioning" "progress_photos" {
+  bucket = aws_s3_bucket.progress_photos.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_public_access_block" "progress_photos" {
+  bucket = aws_s3_bucket.progress_photos.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
--- terraform/main/progress_photo_s3/output.tf
+++ terraform/main/progress_photo_s3/output.tf
@@ -1,3 +1,3 @@
 output "bucket_name" {
-    value = aws_s3_bucket.terraform_state.bucket
+    value = aws_s3_bucket.progress_photos.bucket
 }
