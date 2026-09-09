55b571cfb47d892f31972eb2f4ca7f5178fa3720
Edgar Mevaa <mevaed4@gmail.com>
2026-09-09 17:51:52 +0100

    CI:testing the CI

--- terraform/bootstrap/iam/main.tf
+++ terraform/bootstrap/iam/main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "github_bootstrap_role" {
   name               = "github-bootstrap-role"
-  assume_role_policy = var.github_oidc_assume_json 
+  assume_role_policy = var.github_oidc_assume_json
 }
 
 resource "aws_iam_policy" "terraform_bootstrap" {
@@ -10,6 +10,11 @@ resource "aws_iam_policy" "terraform_bootstrap" {
     Version = "2012-10-17"
 
     Statement = [
+
+      # ============================================================
+      # RDS
+      # ============================================================
+
       {
         Sid    = "RDSInstanceLifecycle"
         Effect = "Allow"
@@ -26,6 +31,7 @@ resource "aws_iam_policy" "terraform_bootstrap" {
         ]
         Resource = "*"
       },
+
       {
         Sid    = "RDSSubnetAndParamGroups"
         Effect = "Allow"
@@ -42,6 +48,28 @@ resource "aws_iam_policy" "terraform_bootstrap" {
         ]
         Resource = "*"
       },
+
+      # ============================================================
+      # EC2 / VPC
+      # ============================================================
+
+      {
+        Sid    = "EC2NetworkingRead"
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeRouteTables",
+          "ec2:DescribeAvailabilityZones"
+        ]
+        Resource = "*"
+      },
+
+      # ============================================================
+      # S3 - Terraform state
+      # ============================================================
+
       {
         Sid    = "TerraformStateBucket"
         Effect = "Allow"
@@ -51,6 +79,7 @@ resource "aws_iam_policy" "terraform_bootstrap" {
         ]
         Resource = "arn:aws:s3:::edgar-fitness-terraform-state"
       },
+
       {
         Sid    = "TerraformStateObject"
         Effect = "Allow"
@@ -63,7 +92,216 @@ resource "aws_iam_policy" "terraform_bootstrap" {
           "arn:aws:s3:::edgar-fitness-terraform-state/fitness-platform/terraform.tfstate",
           "arn:aws:s3:::edgar-fitness-terraform-state/fitness-platform/terraform.tfstate.tflock"
         ]
-          
+      },
+
+      # ============================================================
+      # S3 - Application buckets
+      # ============================================================
+
+      {
+        Sid    = "S3BucketLifecycle"
+        Effect = "Allow"
+        Action = [
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:GetBucketLocation",
+          "s3:GetBucketVersioning",
+          "s3:PutBucketVersioning",
+          "s3:GetPublicAccessBlock",
+          "s3:PutPublicAccessBlock",
+          "s3:DeletePublicAccessBlock",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:DeleteBucketPolicy",
+          "s3:GetBucketTagging",
+          "s3:PutBucketTagging"
+        ]
+        Resource = "*"
+      },
+
+      # ============================================================
+      # KMS
+      # ============================================================
+
+      {
+        Sid    = "KMSKeyLifecycle"
+        Effect = "Allow"
+        Action = [
+          "kms:CreateKey",
+          "kms:DescribeKey",
+          "kms:EnableKey",
+          "kms:DisableKey",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion",
+          "kms:EnableKeyRotation",
+          "kms:DisableKeyRotation",
+          "kms:PutKeyPolicy",
+          "kms:GetKeyPolicy",
+          "kms:ListKeyPolicies",
+          "kms:TagResource",
+          "kms:UntagResource"
+        ]
+        Resource = "*"
+      },
+
+      {
+        Sid    = "KMSAliasLifecycle"
+        Effect = "Allow"
+        Action = [
+          "kms:CreateAlias",
+          "kms:UpdateAlias",
+          "kms:DeleteAlias",
+          "kms:ListAliases"
+        ]
+        Resource = "*"
+      },
+
+      # ============================================================
+      # Secrets Manager
+      # ============================================================
+
+      {
+        Sid    = "SecretsManagerLifecycle"
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:CreateSecret",
+          "secretsmanager:DeleteSecret",
+          "secretsmanager:RestoreSecret",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:PutSecretValue",
+          "secretsmanager:UpdateSecret",
+          "secretsmanager:UpdateSecretVersionStage",
+          "secretsmanager:TagResource",
+          "secretsmanager:UntagResource"
+        ]
+        Resource = "*"
+      },
+
+      # ============================================================
+      # SNS
+      # ============================================================
+
+      {
+        Sid    = "SNSLifecycle"
+        Effect = "Allow"
+        Action = [
+          "sns:CreateTopic",
+          "sns:DeleteTopic",
+          "sns:GetTopicAttributes",
+          "sns:SetTopicAttributes",
+          "sns:TagResource",
+          "sns:UntagResource",
+          "sns:ListTags"
+        ]
+        Resource = "*"
+      },
+
+      # ============================================================
+      # SQS
+      # ============================================================
+
+      {
+        Sid    = "SQSLifecycle"
+        Effect = "Allow"
+        Action = [
+          "sqs:CreateQueue",
+          "sqs:DeleteQueue",
+          "sqs:GetQueueAttributes",
+          "sqs:SetQueueAttributes",
+          "sqs:GetQueueUrl",
+          "sqs:TagQueue",
+          "sqs:UntagQueue"
+        ]
+        Resource = "*"
+      },
+
+      # ============================================================
+      # ElastiCache
+      # ============================================================
+
+      {
+        Sid    = "ElastiCacheLifecycle"
+        Effect = "Allow"
+        Action = [
+          "elasticache:CreateCacheCluster",
+          "elasticache:DeleteCacheCluster",
+          "elasticache:ModifyCacheCluster",
+          "elasticache:DescribeCacheClusters",
+          "elasticache:CreateReplicationGroup",
+          "elasticache:DeleteReplicationGroup",
+          "elasticache:ModifyReplicationGroup",
+          "elasticache:DescribeReplicationGroups",
+          "elasticache:CreateCacheSubnetGroup",
+          "elasticache:DeleteCacheSubnetGroup",
+          "elasticache:ModifyCacheSubnetGroup",
+          "elasticache:DescribeCacheSubnetGroups",
+          "elasticache:AddTagsToResource",
+          "elasticache:RemoveTagsFromResource",
+          "elasticache:ListTagsForResource"
+        ]
+        Resource = "*"
+      },
+
+      # ============================================================
+      # Lambda
+      # ============================================================
+
+      {
+        Sid    = "LambdaLifecycle"
+        Effect = "Allow"
+        Action = [
+          "lambda:CreateFunction",
+          "lambda:DeleteFunction",
+          "lambda:UpdateFunctionCode",
+          "lambda:UpdateFunctionConfiguration",
+          "lambda:GetFunction",
+          "lambda:GetFunctionConfiguration",
+          "lambda:PublishVersion",
+          "lambda:DeleteFunctionUrlConfig",
+          "lambda:CreateFunctionUrlConfig",
+          "lambda:UpdateFunctionUrlConfig",
+          "lambda:AddPermission",
+          "lambda:RemovePermission",
+          "lambda:TagResource",
+          "lambda:UntagResource",
+          "lambda:ListTags",
+          "lambda:ListVersionsByFunction"
+        ]
+        Resource = "*"
+      },
+
+      # ============================================================
+      # IAM - Lambda execution roles
+      # ============================================================
+
+      {
+        Sid    = "LambdaExecutionRoleLifecycle"
+        Effect = "Allow"
+        Action = [
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:GetRole",
+          "iam:UpdateRole",
+          "iam:PutRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+          "iam:TagRole",
+          "iam:UntagRole"
+        ]
+        Resource = "*"
+      },
+
+      {
+        Sid    = "LambdaPassRole"
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = "*"
       }
     ]
   })
