fe3bad90e7825caf01e6db8918fe1cad7ba118fd
Edgar Mevaa <mevaed4@gmail.com>
2026-09-10 17:02:06 +0100

    CI: testing the CI

--- terraform/main/output.tf
+++ terraform/main/output.tf
@@ -2,3 +2,8 @@ output "kms_key_id" {
   description = "ID of the KMS credentials key."
   value       = module.kms.key_id
 }
+
+output "repository_url" {
+  description = "ID of the KMS credentials key."
+  value       = "110"
+}
