50f44a22b1ebcc9c96e801b7e3101d92474e2671
Edgar Mevaa <mevaed4@gmail.com>
2026-09-10 16:20:24 +0100

    CI: testing the CI

--- terraform/bootstrap/iam/main.tf
+++ terraform/bootstrap/iam/main.tf
@@ -106,6 +106,7 @@ resource "aws_iam_policy" "terraform_bootstrap" {
           "s3:GetBucketTagging",
           "s3:GetBucketAcl",
           "s3:GetBucketCORS",
+          "s3:GetBucketWebsite"
         ]
         Resource = "arn:aws:s3:::edgar-fitness-progess-photos"
       },
